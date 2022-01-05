8f1b8fb5bb40f004e2f8f8eee931abbacf02d65a
CannedShroud <hypersync112@gmail.com>
2022-01-05 18:52:37 +0000

    changes for lisa

--- anykernel.sh
+++ anykernel.sh
@@ -10,8 +10,8 @@ do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=1
-device.name1=raphael
-device.name2=raphaelin
+device.name1=lisa
+device.name2=lisain
 device.name3=
 device.name4=
 device.name5=
@@ -33,38 +33,6 @@ no_block_display=true;
 
 ## AnyKernel install
 dump_boot;
-
-case "$ZIPFILE" in
-  *69fps*|*69hz*)
-    ui_print "  • Setting 69 Hz refresh rate"
-    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=1"
-    ;;
-  *72fps*|*72hz*)
-    ui_print "  • Setting 72 Hz refresh rate"
-    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=2"
-    ;;
-  *75fps*|*75hz*)
-    ui_print "  • Setting 75 Hz refresh rate"
-    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=3"
-    ;;
-  *78fps*|*78hz*)
-    ui_print "  • Setting 78 Hz refresh rate"
-    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=4"
-    ;;
-  *81fps*|*81hz*)
-    ui_print "  • Setting 81 Hz refresh rate"
-    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=5"
-    ;;
-  *)
-    patch_cmdline "msm_drm.framerate_override" ""
-    fr=$(cat /sdcard/framerate_override | tr -cd "[0-9]");
-    [ $fr -eq 69 ] && ui_print "  • Setting 69 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=1"
-    [ $fr -eq 72 ] && ui_print "  • Setting 72 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=2"
-    [ $fr -eq 75 ] && ui_print "  • Setting 75 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=3"
-    [ $fr -eq 78 ] && ui_print "  • Setting 78 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=4"
-    [ $fr -eq 81 ] && ui_print "  • Setting 81 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=5"
-    ;;
-esac
 
 write_boot;
 ## end install
